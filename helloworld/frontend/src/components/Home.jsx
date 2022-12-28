7e8c4bcd90e306b110b3d12c7a403a211f05d384
jli511 <jli511@ucsc.edu>
2022-12-28 13:56:36 -0800

    add frontend

---
+++ helloworld/frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FaceIcon from './faceIcon';
+import Socials from './socials';
+import './home.css';
+function Home() {
+    return (
+        <div>
+            <FaceIcon />
+            <Socials />
+        </div>
+    );
+};
+
+export default Home;
---
+++ helloworld/frontend/src/components/faceIcon.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+
+function FaceIcon() {
+    return (
+        <div>
+            <Avatar id='avatar'
+            src='https://media.discordapp.net/attachments/368534832197664769/1057742297997000715/d3b55e2b466e91c4.jpg?width=525&height=676'/>
+        </div>
+    );
+};
+
+export default FaceIcon;
