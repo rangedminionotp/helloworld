b8c726cdc85ef3cf2d33324385da14e756d3a3f0
Jialin Li <jli511@ucsc.edu>
2023-02-22 19:20:26 -0800

    more css to about me

--- src/components/aboutme/content/experience.jsx
+++ src/components/aboutme/content/experience.jsx
@@ -10,12 +10,14 @@ function Experience() {
             </div>
             <div id='aboutmeFI'>
                 <FaceIcon />
+                <div class='greeting myname'>About Me</div>
             </div>
-            
-            <div id='aboutmeContent'>
+            <div id='aboutmeContent'> 
+                <div id='aboutmetext'>
                 <p>Hi~ My name is Jade Li, currently a fourth year Computer Science major at UC Santa Cruz.</p>
-                <p>I will graduate June 2023 with a B.S. degree =w=</p>
+                <p>I will graduate in June 2023 with a B.S. degree =w=</p>
                 <p>I took a variety of CS courses, as of now I'm mainly doing UI and NERP full-stack web development projects.</p>
+                </div>
             </div>
         </div>
     );
